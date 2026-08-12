618e03dc3ba63f297a57048154b1c0ba08ca1bea
KrovvidiSameera <krovvidisameera.25.csm@anits.edu.in>
2026-08-12 22:26:42 +0530

    Implement basic bank account functionality

--- Record Programs/Week-01/BankSystem.java
+++ Record Programs/Week-01/BankSystem.java
@@ -1,1 +1,37 @@
+class Account {
 
+    int accountNumber;
+    String accountType;
+    double balance;
+
+    void deposit(double amount) {
+        balance = balance + amount;
+        System.out.println("Deposited: " + amount);
+    }
+    void withdraw(double amount) {
+        if (amount <= balance) {
+            balance = balance - amount;
+            System.out.println("Withdrawn: " + amount);
+        } else {
+            System.out.println("Insufficient balance");
+        }
+    }
+    void display() {
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Account Type: " + accountType);
+        System.out.println("Balance: " + balance);
+    }
+}
+public class BankMangementSystem {
+    public static void main(String[] args) {
+        Account a = new Account();
+        a.accountNumber = 101;
+        a.accountType = "Savings";
+        a.balance = 5000;
+        a.display();
+        a.deposit(2000);
+        a.withdraw(1000);
+        System.out.println("\nAfter Transactions:");
+        a.display();
+    }
+}
