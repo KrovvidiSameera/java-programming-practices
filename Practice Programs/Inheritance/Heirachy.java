1e7a9f47d2670556e91929142dabcf6bfa407128
KrovvidiSameera <krovvidisameera.25.csm@anits.edu.in>
2026-08-12 22:29:19 +0530

    Update Heirachy.java

--- Practice Programs/Inheritance/Heirachy.java
+++ Practice Programs/Inheritance/Heirachy.java
@@ -3,26 +3,21 @@ class Animal {
         System.out.println("Animal eats");
     }
 }
-
 class Dog extends Animal {
     void bark() {
         System.out.println("Dog barks");
     }
 }
-
 class Cat extends Animal {
     void meow() {
         System.out.println("Cat meows");
     }
 }
-
 public class HierarchicalInheritance {
     public static void main(String[] args) {
-
         Dog d = new Dog();
         d.eat();
         d.bark();
-
         Cat c = new Cat();
         c.eat();
         c.meow();
