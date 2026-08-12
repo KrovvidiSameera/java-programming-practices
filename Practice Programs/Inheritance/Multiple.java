7f487f274afe50dadfcb8556903dff60bd9a1f75
KrovvidiSameera <krovvidisameera.25.csm@anits.edu.in>
2026-08-12 22:12:07 +0530

    Update Multiple.java

--- Practice Programs/Inheritance/Multiple.java
+++ Practice Programs/Inheritance/Multiple.java
@@ -1,1 +1,26 @@
+interface Father {
+    void work();
+}
 
+interface Mother {
+    void cook();
+}
+
+class Child implements Father, Mother {
+
+    public void work() {
+        System.out.println("Father works");
+    }
+
+    public void cook() {
+        System.out.println("Mother cooks");
+    }
+
+    public static void main(String[] args) {
+
+        Child c = new Child();
+
+        c.work();
+        c.cook();
+    }
+}
