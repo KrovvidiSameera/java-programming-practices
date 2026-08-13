94862fd1672f3d823a2a9c4078a6132a19edff5c
KrovvidiSameera <krovvidisameera.25.csm@anits.edu.in>
2026-08-13 22:23:41 +0530

    Implement inheritance and interfaces in Hybrid.java

--- Assignment Programs/Assignment-01/Hybrid.java
+++ Assignment Programs/Assignment-01/Hybrid.java
@@ -1,1 +1,38 @@
+package javacore;
+interface Pet {
+    void play();
+}
 
+class Animal {
+    void eat() {
+        System.out.println("Animal eats");
+    }
+}
+
+class Dog extends Animal implements Pet {
+    void bark() {
+        System.out.println("Dog barks");
+    }
+
+    public void play() {
+        System.out.println("Dog plays");
+    }
+}
+
+class Puppy extends Dog {
+    void sleep() {
+        System.out.println("Puppy sleeps");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Puppy p = new Puppy();
+
+        p.eat();
+        p.bark();
+        p.play();
+        p.sleep();
+    }
+}
