0512b880d71127ab866d35473ed0aa752a63962f
KrovvidiSameera <krovvidisameera.25.csm@anits.edu.in>
2026-08-13 22:10:17 +0530

    Add Animal and Dog classes with methods

--- Assignment Programs/Assignment-01/Single.java
+++ Assignment Programs/Assignment-01/Single.java
@@ -1,1 +1,19 @@
+package javacore;
+class Animal {
+    void eat() {
+        System.out.println("Animal eats");
+    }
+}
 
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog barks");
+    }
+
+    public static void main(String[] args) {
+        Dog d = new Dog();
+
+        d.eat();
+        d.bark();
+    }
+}
