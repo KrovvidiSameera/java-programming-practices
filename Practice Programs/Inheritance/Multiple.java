30c43f339ec11707e29238cad311ca4b0c3b5f48
KrovvidiSameera <krovvidisameera.25.csm@anits.edu.in>
2026-08-13 22:17:46 +0530

    Fix class declaration and formatting in Multiple.java

--- Practice Programs/Inheritance/Multiple.java
+++ Practice Programs/Inheritance/Multiple.java
@@ -1,16 +1,23 @@
 package javacore;
+
 interface Father {
+    int age = 50;
     void work();
 }
+
 interface Mother {
+    int age = 45;
     void cook();
 }
-class Child implements Father, Mother {
+
+ public class Child implements Father, Mother {
     public void work() {
         System.out.println("Father works");
+        System.out.println("Father age: " + Father.age);
     }
     public void cook() {
         System.out.println("Mother cooks");
+        System.out.println("Mother age: " + Mother.age);
     }
     public static void main(String[] args) {
         Child c = new Child();
