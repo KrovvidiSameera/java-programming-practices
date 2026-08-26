9f05bfafce3ea0a06718a3e953f69e6b4f176360
KrovvidiSameera <krovvidisameera.25.csm@anits.edu.in>
2026-08-26 12:55:56 +0530

    Rename class from InterfaceImplementation to C1

--- Assignment Programs/Assignment-02/C1ImplementsI1,I2.java
+++ Assignment Programs/Assignment-02/C1ImplementsI1,I2.java
@@ -10,7 +10,7 @@ interface I1{
     void sample();
  }
 
-public class InterfaceImplementation implements I1,I2{
+public class C1 implements I1,I2{
     @Override
     public void demo() {
         System.out.println("this is the abstract method of interface1");
@@ -21,7 +21,7 @@ public class InterfaceImplementation implements I1,I2{
          System.out.println("this is the abstract method of interface 2");
     }
     public static void main(String[] args){
-        InterfaceImplementation ac=new InterfaceImplementation();
+        C1 ac=new C1();
         System.out.println(ac.a);
         System.out.println(ac.b);
         ac.demo();
