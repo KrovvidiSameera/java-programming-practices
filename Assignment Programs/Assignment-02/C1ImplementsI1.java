f985fcd58d3812d22ed0d0de916578fdda5c82a8
KrovvidiSameera <krovvidisameera.25.csm@anits.edu.in>
2026-08-26 12:56:25 +0530

    Implement interface I1 in class C1

--- Assignment Programs/Assignment-02/C1ImplementsI1.java
+++ Assignment Programs/Assignment-02/C1ImplementsI1.java
@@ -1,1 +1,22 @@
+package javacore;
+interface I1{
+    int a=25;
+
+    void demo();
+}
+
+public class C1 implements I1{
+
+    @Override
+    public void demo() {
+        System.out.println("this is the abstract method of interface1");
+
+    }
+    public static void main(String[] args){
+        C1 ac=new C1();
+        System.out.println(ac.a);
+        ac.demo();
+
+    }
+}
 
