76d8479bd1aecc8f660d55039261821011cb7975
KrovvidiSameera <krovvidisameera.25.csm@anits.edu.in>
2026-09-10 20:46:09 +0530

    Update StringReplace.java

--- Assignment Programs/Assignment-03/StringReplace.java
+++ Assignment Programs/Assignment-03/StringReplace.java
@@ -1,1 +1,10 @@
+class ReplaceExample {
+    public static void main(String[] args) {
+        String str = "Hello";
 
+        String result = str.replace('l', 'x');
+
+        System.out.println("Original: " + str);
+        System.out.println("After replacement: " + result);
+    }
+}
