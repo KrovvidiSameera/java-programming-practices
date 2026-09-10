37468da7bde550c5b2bf09bc0ef6f0f17d042787
KrovvidiSameera <krovvidisameera.25.csm@anits.edu.in>
2026-09-10 21:25:33 +0530

    Remove unnecessary whitespace in StringIndexOf.java

--- Assignment Programs/Assignment-03/StringIndexOf.java
+++ Assignment Programs/Assignment-03/StringIndexOf.java
@@ -2,9 +2,7 @@ package stringmethods;
 class IndexOfExample {
     public static void main(String[] args) {
         String str = "Hello Java";
-
         int result = str.indexOf('J');
-
         System.out.println("String: " + str);
         System.out.println("Index of J: " + result);
     }
