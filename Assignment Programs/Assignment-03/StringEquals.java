65a9ca5bac1eb4e4cbb77ae19c08112ab2afa544
KrovvidiSameera <krovvidisameera.25.csm@anits.edu.in>
2026-09-10 20:43:28 +0530

    Update StringEquals.java

--- Assignment Programs/Assignment-03/StringEquals.java
+++ Assignment Programs/Assignment-03/StringEquals.java
@@ -1,1 +1,12 @@
+class EqualsExample {
+    public static void main(String[] args) {
+        String str1 = "Java";
+        String str2 = "Java";
 
+        boolean result = str1.equals(str2);
+
+        System.out.println("First String: " + str1);
+        System.out.println("Second String: " + str2);
+        System.out.println("Are they equal? " + result);
+    }
+}
