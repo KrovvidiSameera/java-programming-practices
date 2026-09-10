34b7cd41e38d6518da20c1e1be782f45c641f709
KrovvidiSameera <krovvidisameera.25.csm@anits.edu.in>
2026-09-10 21:24:39 +0530

    Update StringCharacter.java

--- Assignment Programs/Assignment-03/StringCharacter.java
+++ Assignment Programs/Assignment-03/StringCharacter.java
@@ -2,7 +2,6 @@ package stringmethods;
 class CharAtExample {
     public static void main(String[] args) {
         String str = "Java";
-
         System.out.println("String: " + str);
         System.out.println("Character at index 2: " + str.charAt(2));
     }
