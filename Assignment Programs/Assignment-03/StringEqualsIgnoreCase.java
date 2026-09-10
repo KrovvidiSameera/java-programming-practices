353d8f5609685c8cbbb3005799410512c1788d60
KrovvidiSameera <krovvidisameera.25.csm@anits.edu.in>
2026-09-10 20:43:49 +0530

    Update StringEqualsIgnoreCase.java

--- Assignment Programs/Assignment-03/StringEqualsIgnoreCase.java
+++ Assignment Programs/Assignment-03/StringEqualsIgnoreCase.java
@@ -1,1 +1,12 @@
+class EqualsIgnoreCaseExample {
+    public static void main(String[] args) {
+        String str1 = "Java";
+        String str2 = "JAVA";
 
+        boolean result = str1.equalsIgnoreCase(str2);
+
+        System.out.println("First String: " + str1);
+        System.out.println("Second String: " + str2);
+        System.out.println("Are they equal? " + result);
+    }
+}
