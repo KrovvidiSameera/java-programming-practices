60e2156adf28ed5ea09803382400c98eb09a8cc5
KrovvidiSameera <krovvidisameera.25.csm@anits.edu.in>
2026-09-10 20:45:37 +0530

    Update StringLength.java

--- Assignment Programs/Assignment-03/StringLength.java
+++ Assignment Programs/Assignment-03/StringLength.java
@@ -1,1 +1,8 @@
+class LengthExample {
+    public static void main(String[] args) {
+        String str = "Hello";
 
+        System.out.println("String: " + str);
+        System.out.println("Length: " + str.length());
+    }
+}
