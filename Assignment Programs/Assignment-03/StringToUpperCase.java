ca38f81a51164edb7ba2c2bf867688653520960f
KrovvidiSameera <krovvidisameera.25.csm@anits.edu.in>
2026-09-10 20:47:47 +0530

    Update StringToUpperCase.java

--- Assignment Programs/Assignment-03/StringToUpperCase.java
+++ Assignment Programs/Assignment-03/StringToUpperCase.java
@@ -1,1 +1,10 @@
+class UpperCaseExample {
+    public static void main(String[] args) {
+        String str = "hello java";
 
+        String result = str.toUpperCase();
+
+        System.out.println("Original: " + str);
+        System.out.println("Uppercase: " + result);
+    }
+}
