86f2e90636f26fe2d2ed94fd9411ed549e6549f2
KrovvidiSameera <krovvidisameera.25.csm@anits.edu.in>
2026-09-10 20:45:10 +0530

    Update StringLastIndexOf.java

--- Assignment Programs/Assignment-03/StringLastIndexOf.java
+++ Assignment Programs/Assignment-03/StringLastIndexOf.java
@@ -1,1 +1,10 @@
+class LastIndexOfExample {
+    public static void main(String[] args) {
+        String str = "Hello";
 
+        int result = str.lastIndexOf('l');
+
+        System.out.println("String: " + str);
+        System.out.println("Last index of l: " + result);
+    }
+}
