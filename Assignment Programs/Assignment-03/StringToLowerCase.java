1b99ed5e9252466e9eecdad534e773f7300c9e92
KrovvidiSameera <krovvidisameera.25.csm@anits.edu.in>
2026-09-10 20:47:23 +0530

    Update StringToLowerCase.java

--- Assignment Programs/Assignment-03/StringToLowerCase.java
+++ Assignment Programs/Assignment-03/StringToLowerCase.java
@@ -1,1 +1,10 @@
+class LowerCaseExample {
+    public static void main(String[] args) {
+        String str = "HELLO JAVA";
 
+        String result = str.toLowerCase();
+
+        System.out.println("Original: " + str);
+        System.out.println("Lowercase: " + result);
+    }
+}
