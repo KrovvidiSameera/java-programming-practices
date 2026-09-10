c262e3a673eaf38aedbe987407f0c351f97fd179
KrovvidiSameera <krovvidisameera.25.csm@anits.edu.in>
2026-09-10 20:46:53 +0530

    Update StringSubstring.java

--- Assignment Programs/Assignment-03/StringSubstring.java
+++ Assignment Programs/Assignment-03/StringSubstring.java
@@ -1,1 +1,10 @@
+class SubstringExample {
+    public static void main(String[] args) {
+        String str = "Hello Java";
 
+        String result = str.substring(6, 10);
+
+        System.out.println("Original: " + str);
+        System.out.println("Substring: " + result);
+    }
+}
