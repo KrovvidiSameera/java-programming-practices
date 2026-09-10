a8f732009f81e1f5888042c2a91288fda32534aa
KrovvidiSameera <krovvidisameera.25.csm@anits.edu.in>
2026-09-10 20:32:04 +0530

    Add CharAtExample class for string character access

--- Assignment Programs/Assignment-03/StringConcatenation.java
+++ Assignment Programs/Assignment-03/StringConcatenation.java
@@ -1,1 +1,8 @@
+class CharAtExample {
+    public static void main(String[] args) {
+        String str = "Java";
 
+        System.out.println("String: " + str);
+        System.out.println("Character at index 2: " + str.charAt(2));
+    }
+}
