8373f7b9d4960a237e82ec70b6de87230da5e61d
KrovvidiSameera <krovvidisameera.25.csm@anits.edu.in>
2026-09-10 20:44:48 +0530

    Update StringIndexOf.java

--- Assignment Programs/Assignment-03/StringIndexOf.java
+++ Assignment Programs/Assignment-03/StringIndexOf.java
@@ -1,1 +1,10 @@
+class IndexOfExample {
+    public static void main(String[] args) {
+        String str = "Hello Java";
 
+        int result = str.indexOf('J');
+
+        System.out.println("String: " + str);
+        System.out.println("Index of J: " + result);
+    }
+}
