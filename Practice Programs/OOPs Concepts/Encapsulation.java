64689c80b9b2ab1ef2d51f74cc1e6fd80e77a100
KrovvidiSameera <krovvidisameera.25.csm@anits.edu.in>
2026-09-15 19:34:13 +0530

    Create Encapsulation.java

---
+++ Practice Programs/OOPs Concepts/Encapsulation.java
@@ -0,0 +1,34 @@
+package javacore;
+
+// Class for encapsulation
+class Sample {
+
+    // Private data can be used for better encapsulation
+    int x;
+    float y;
+    char c;
+    String k;
+
+    // Getter for x
+    public int getA() {
+        return x;
+    }
+
+    // Setter for x
+    public void setA(int x) {
+        this.x = x;
+    }
+
+    // Getter for y
+    public float getB() {
+        return y;
+    }
+
+    // Setter for y
+    public void setB(float y) {
+        this.y = y;
+    }
+
+    // Getter for c
+    public char getC() {
+        return c;
