9749583d51259b7e3afeb33cd395a3171d31122f
KrovvidiSameera <krovvidisameera.25.csm@anits.edu.in>
2026-09-23 18:36:34 +0530

    Update SortedSetInterface.java

--- ObservationPrograms/Task-06/SortedSetInterface/SortedSetInterface.java
+++ ObservationPrograms/Task-06/SortedSetInterface/SortedSetInterface.java
@@ -1,1 +1,37 @@
+package collectionframework;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+    public class SortedSetExample {
+        public static void main(String[] args) {
+
+            SortedSet<Integer> set = new TreeSet<>();
+
+            set.add(50);
+            set.add(20);
+            set.add(40);
+            set.add(10);
+            set.add(30);
+
+            System.out.println("Sorted Set: " + set);
+
+            // first()
+            System.out.println("First: " + set.first());
+
+            // last()
+            System.out.println("Last: " + set.last());
+
+            // headSet()
+            System.out.println("HeadSet: " + set.headSet(30));
+
+            // tailSet()
+            System.out.println("TailSet: " + set.tailSet(30));
+
+            // subSet()
+            System.out.println("SubSet: " + set.subSet(20, 50));
+
+            // comparator()
+            System.out.println("Comparator: " + set.comparator());
+        }
+    }
 
