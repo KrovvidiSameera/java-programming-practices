933140b51b8f389222c3a6a4e9b5876e257c3f46
KrovvidiSameera <krovvidisameera.25.csm@anits.edu.in>
2026-09-23 18:29:55 +0530

    Update SetInterface.java

--- ObservationPrograms/Task-06/SetInterface/SetInterface.java
+++ ObservationPrograms/Task-06/SetInterface/SetInterface.java
@@ -1,1 +1,36 @@
+package collectionframework;
+import java.util.Set;
+import java.util.HashSet;
+
+    public class SetExample {
+        public static void main(String[] args) {
+
+            Set<String> set = new HashSet<>();
+
+            // add()
+            set.add("Apple");
+            set.add("Banana");
+            set.add("Mango");
+            set.add("Apple");     // Duplicate
+
+            System.out.println("Set: " + set);
+
+            // remove()
+            set.remove("Banana");
+            System.out.println("After remove: " + set);
+
+            // contains()
+            System.out.println("Contains Mango: " + set.contains("Mango"));
+
+            // size()
+            System.out.println("Size: " + set.size());
+
+            // isEmpty()
+            System.out.println("Is Empty: " + set.isEmpty());
+
+            // clear()
+            set.clear();
+            System.out.println("After clear: " + set);
+        }
+    }
 
