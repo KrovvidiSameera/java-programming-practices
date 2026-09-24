797f82c1865cd9b3573eee2e487a8a16e92ae229
KrovvidiSameera <krovvidisameera.25.csm@anits.edu.in>
2026-09-24 18:35:34 +0530

    Update HashSetClass.java

--- ObservationPrograms/Task-07/HashSetClass/HashSetClass.java
+++ ObservationPrograms/Task-07/HashSetClass/HashSetClass.java
@@ -1,1 +1,37 @@
+package collectionframework;
+import java.util.HashSet;
+
+    public class HashSetExample {
+        public static void main(String[] args) {
+
+            HashSet<String> set = new HashSet<>();
+
+            // add()
+            set.add("Apple");
+            set.add("Banana");
+            set.add("Mango");
+            set.add("Apple");   // Duplicate
+
+            System.out.println("HashSet: " + set);
+
+            // remove()
+            set.remove("Banana");
+            System.out.println("After remove: " + set);
+
+            // contains()
+            System.out.println("Contains Mango: " +
+                    set.contains("Mango"));
+
+            // size()
+            System.out.println("Size: " + set.size());
+
+            // isEmpty()
+            System.out.println("Is Empty: " + set.isEmpty());
+
+            // clear()
+            set.clear();
+
+            System.out.println("After clear: " + set);
+        }
+    }
 
