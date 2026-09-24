5476ffe9cec7789078f3ddde3c3b4be6af889525
KrovvidiSameera <krovvidisameera.25.csm@anits.edu.in>
2026-09-24 18:38:05 +0530

    Update LinkedHashSetClass.java

--- ObservationPrograms/Task-07/LinkedHashSetClass/LinkedHashSetClass.java
+++ ObservationPrograms/Task-07/LinkedHashSetClass/LinkedHashSetClass.java
@@ -1,1 +1,33 @@
+package collectionframework;
+import java.util.LinkedHashSet;
 
+    public class LinkedHashSetExample {
+        public static void main(String[] args) {
+
+            LinkedHashSet<String> set = new LinkedHashSet<>();
+
+            // add()
+            set.add("Apple");
+            set.add("Banana");
+            set.add("Mango");
+            set.add("Apple");  // Duplicate
+
+            System.out.println("LinkedHashSet: " + set);
+
+            // remove()
+            set.remove("Banana");
+            System.out.println("After remove: " + set);
+
+            // contains()
+            System.out.println("Contains Apple: " +
+                    set.contains("Apple"));
+
+            // size()
+            System.out.println("Size: " + set.size());
+
+            // clear()
+            set.clear();
+
+            System.out.println("After clear: " + set);
+        }
+    }
