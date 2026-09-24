a4748a53fd5c96082c18b2680b69f697a221aa5b
KrovvidiSameera <krovvidisameera.25.csm@anits.edu.in>
2026-09-24 13:42:41 +0530

    Update ListIteratorInterface(1).java

--- ObservationPrograms/Task-06/ListIteratorInterface(1)/ListIteratorInterface(1).java
+++ ObservationPrograms/Task-06/ListIteratorInterface(1)/ListIteratorInterface(1).java
@@ -1,1 +1,33 @@
+package collectionframework;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.ListIterator;
 
+    public class ListIteratorExample {
+        public static void main(String[] args) {
+
+            List<String> list = new ArrayList<>();
+
+            list.add("Apple");
+            list.add("Banana");
+            list.add("Mango");
+
+            ListIterator<String> it = list.listIterator();
+
+            // Forward direction
+            System.out.println("Forward:");
+
+            while (it.hasNext()) {
+
+                System.out.println(it.next());
+            }
+
+            // Backward direction
+            System.out.println("\nBackward:");
+
+            while (it.hasPrevious()) {
+
+                System.out.println(it.previous());
+            }
+        }
+    }
