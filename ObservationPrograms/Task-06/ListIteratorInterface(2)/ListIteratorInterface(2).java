bc443bf62a433a38411f64b2a556db1bd71c01b0
KrovvidiSameera <krovvidisameera.25.csm@anits.edu.in>
2026-09-24 13:45:31 +0530

    Update ListIteratorInterface(2).java

--- ObservationPrograms/Task-06/ListIteratorInterface(2)/ListIteratorInterface(2).java
+++ ObservationPrograms/Task-06/ListIteratorInterface(2)/ListIteratorInterface(2).java
@@ -1,1 +1,41 @@
+package collectionframework;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+    public class ListIteratorMethods {
+        public static void main(String[] args) {
+
+            List<String> list = new ArrayList<>();
+
+            list.add("Apple");
+            list.add("Banana");
+            list.add("Mango");
+
+            ListIterator<String> it = list.listIterator();
+
+            // Move to Banana
+            it.next();
+            it.next();
+
+            // set()
+            it.set("Orange");
+
+            System.out.println("After set: " + list);
+
+            // add()
+            it.add("Grapes");
+
+            System.out.println("After add: " + list);
+
+            // Move to Grapes
+            it.previous();
+
+            // remove()
+            it.remove();
+
+            System.out.println("After remove: " + list);
+        }
+    }
+
 
