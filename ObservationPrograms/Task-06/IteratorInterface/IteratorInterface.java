a192195f95a4e8909be6580721d898e2aa1e5cc1
KrovvidiSameera <krovvidisameera.25.csm@anits.edu.in>
2026-09-24 13:39:13 +0530

    Update IteratorInterface.java

--- ObservationPrograms/Task-06/IteratorInterface/IteratorInterface.java
+++ ObservationPrograms/Task-06/IteratorInterface/IteratorInterface.java
@@ -1,1 +1,32 @@
+package collectionframework;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ArrayList;
+
+    public class IteratorExample {
+        public static void main(String[] args) {
+
+            List<String> list = new ArrayList<>();
+
+            list.add("Apple");
+            list.add("Banana");
+            list.add("Mango");
+
+            Iterator<String> it = list.iterator();
+
+            while (it.hasNext()) {
+
+                String item = it.next();
+
+                System.out.println(item);
+
+                // Remove Banana
+                if (item.equals("Banana")) {
+                    it.remove();
+                }
+            }
+
+            System.out.println("After removal: " + list);
+        }
+    }
 
