0d63f2aa89b50c62a405698fde638ea093b33d8e
KrovvidiSameera <krovvidisameera.25.csm@anits.edu.in>
2026-09-24 13:17:01 +0530

    Update DequeInterface.java

--- ObservationPrograms/Task-06/DequeInterface/DequeInterface.java
+++ ObservationPrograms/Task-06/DequeInterface/DequeInterface.java
@@ -1,1 +1,51 @@
+package collectionframework;
+import java.util.Deque;
+import java.util.ArrayDeque;
 
+    public class DequeExample {
+        public static void main(String[] args) {
+
+            Deque<String> dq = new ArrayDeque<>();
+
+            // addFirst()
+            dq.addFirst("B");
+
+            // addLast()
+            dq.addLast("C");
+
+            dq.addFirst("A");
+            dq.addLast("D");
+
+            System.out.println("Deque: " + dq);
+
+            // offerFirst()
+            dq.offerFirst("Start");
+
+            // offerLast()
+            dq.offerLast("End");
+
+            System.out.println("After offer: " + dq);
+
+            // peekFirst()
+            System.out.println("First: " + dq.peekFirst());
+
+            // peekLast()
+            System.out.println("Last: " + dq.peekLast());
+
+            // pollFirst()
+            System.out.println("Removed First: " + dq.pollFirst());
+
+            // pollLast()
+            System.out.println("Removed Last: " + dq.pollLast());
+
+            System.out.println("Deque after removal: " + dq);
+
+            // removeFirst()
+            dq.removeFirst();
+
+            // removeLast()
+            dq.removeLast();
+
+            System.out.println("Final Deque: " + dq);
+        }
+    }
